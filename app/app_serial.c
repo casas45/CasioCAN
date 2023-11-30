fb49bde5386c9a4e7939692f58541727bdcde1e8
Luis Casas <luis_david_casas@hotmail.com>
2023-11-29 21:03:51 -0600

    app_serial files added

---
+++ app/app_serial.c
@@ -0,0 +1,11 @@
+#include "app_serial.h"
+
+void Serial_InitTask( void )
+{
+
+}
+
+void Serial_PeriodicTask( void )
+{
+    
+}
---
+++ app/app_serial.h
@@ -0,0 +1,8 @@
+#ifndef SERIAL_H__
+#define SERIAL_H__
+
+void Serial_InitTask( void );
+
+void Serial_PeriodicTask( void );
+
+#endif
